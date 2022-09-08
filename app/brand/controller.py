5dd20197ee3950c53ef4c95fd00f56353057c740
KIM <km0192@naver.com>
2022-09-08 14:58:34 +0900

    feat: add CRUD in brand

--- app/brand/controller.py
+++ app/brand/controller.py
@@ -1,8 +1,8 @@
 from typing import List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
-from brand.schema import BrandSchema
+from brand.schema import BrandSchema, UpdateBrandSchema
 from db.db import db
 
 brand_router = APIRouter(
@@ -16,3 +16,43 @@ async def get_brand():
     brands = await db["brand"].find().to_list(100)
 
     return brands
+
+@brand_router.post(
+    "/",
+    response_model=BrandSchema,
+    status_code=201
+)
+async def create_brand(brand:UpdateBrandSchema):
+    brand = dict(brand)
+    db_object = await db["brand"].insert_one(brand)
+    brand["_id"] = str(db_object.inserted_id)
+    
+    return brand
+
+@brand_router.delete(
+    "/{_id}",
+    status_code=204
+)
+async def delete_brand(_id:str):
+    delete_result = await db["brand"].delete_one({"_id":_id})
+
+    if delete_result.deleted_count == 1:
+        return "success"
+
+@brand_router.put(
+    "/{_id}",
+    status_code=204
+)
+async def patch_brand(_id:str, brand:UpdateBrandSchema):
+    new_brand = dict(brand)
+    update_result = await db["brand"].update_one({"_id": id}, {"$set": brand})
+    
+    if (
+        (len(new_brand) > 0) and 
+        (update_result.modified_count == 1) and 
+        (updated_student := await db["brand"].find_one({"_id": id})) is not None
+        ):
+            return updated_student    
+
+    raise HTTPException(satus_code=404, detail="Item not found")
+    
